29fa7c9c42c81ec472f0ecce6f817a1e596cc7a2
Yubo Wang <50394396+Wyb0627@users.noreply.github.com>
2021-11-24 13:31:28 +0800

    Add files via upload

---
+++ GRU_YuboWang/model.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+
+
+class GRUModel(nn.Module):
+    def __init__(self):
+        super(GRUModel, self).__init__()
+        # inputs (batch_size,96,31)
+        self.encoder0 = nn.GRU(input_size=96, hidden_size=96, num_layers=2, dropout=0.1, batch_first=True)
+        self.fc0 = nn.Linear(96, 1)
+
+        self.encoder = nn.GRU(input_size=31, hidden_size=31, num_layers=2, dropout=0.1, batch_first=True)
+        self.fc = nn.Linear(31, 2)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def forward(self, tensor):
+        tensor = tensor.permute(0, 2, 1)
+        out0, hn0 = self.encoder0(tensor)
+        result0 = self.fc0(out0)
+        result0 = result0.permute(0, 2, 1)
+        out, hn = self.encoder(result0)
+        result = self.fc(out)
+        return result
